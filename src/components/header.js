6c75f779192c7231d21aba379f5812af0b9fc9c4
Aran <aranbruce@gmail.com>
2019-06-03 22:52:52 +0100

    added smooth-scroll and gave header functionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,94 +1,81 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'gatsby'
 import Logo from '../images/logo.svg'
+import classnames from "classnames";
 
-const Header = () => {
-    return (
-        <nav id={'nav'}>
-            <div className='nav__content'>
-                <div className='nav__left'>
-                    <Link className='nav__item' to='/'>
-                        <img src={Logo} alt='website logo'/>
-                    </Link>
-                </div>
-                <div className='nav__right'>
-                    <div className='burger-container' id={'burger'}>
-                        <div className='allBars'>
-                            <div className='bar1'></div>
-                            <div className='bar2'></div>
-                            <div className='bar3'></div>
-                        </div>
-                    </div>
-                    <div className='menu' id={'menu'}>
-                        <Link to='/#work' className='nav__item'>Work</Link>
-                        <Link to='/#skills'className='nav__item'>Skills</Link>
-                        <Link to='/#about-me' className='nav__item'>About Me</Link>
-                        <Link to='/#form' className='nav__item'>
-                            <button className='button--secondary'>Get In Touch</button>
+
+export default class Header extends Component {
+    constructor() {
+        super()
+        this.state = {
+            scrolled: false,
+            burgerOpen: false
+          };
+    }
+    toggleBurgerMenu(e){
+        this.setState({burgerOpen: !this.state.burgerOpen});
+     }
+    handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const scrolled = currentScrollPos > 0;
+        this.setState({
+            scrolled
+        });
+    };
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll)
+        window.addEventListener("burger toggle", this.toggleBurgerMenu);
+    } 
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+        window.removeEventListener("burger toggle", this.toggleBurgerMenu);
+    }
+    
+    render() {
+        return (
+            <nav
+                className={classnames('', {
+                'nav--scroll': this.state.scrolled
+                })}
+            >
+                <div className='nav__content'>
+                    <div className='nav__left'>
+                        <Link className='nav__item' to='/'>
+                            <img src={Logo} alt='website logo'/>
                         </Link>
                     </div>
+                    <div className='nav__right'>
+                        <div
+                            className={classnames('burger-container',
+                                {
+                                    'burger-container--scroll': this.state.scrolled
+                                } ||
+                                'burger-container', this.state.burgerOpen ? 'burger-container--close' : ''
+                              )}
+                              onClick={e => this.toggleBurgerMenu(e)}
+                        >
+                            <div className='allBars'>
+                                <div className='bar1'></div>
+                                <div className='bar2'></div>
+                                <div className='bar3'></div>
+                            </div>
+                        </div>
+                        <div
+                            className={classnames('menu',
+                                this.state.burgerOpen ? 'show' : ''
+                            )}
+                        >
+                            <Link to='/#work' className='nav__item'onClick={e => this.toggleBurgerMenu(e)}>Work</Link>
+                            <Link to='/#skills'className='nav__item'onClick={e => this.toggleBurgerMenu(e)}>Skills</Link>
+                            <Link to='/#about-me' className='nav__item'onClick={e => this.toggleBurgerMenu(e)}>About Me</Link>
+                            <Link to='/#form' className='nav__item'onClick={e => this.toggleBurgerMenu(e)}>
+                                <button className='button--secondary'>Get In Touch</button>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </nav>
-        
-    )
+            </nav>
+            
+        )
+    }
 }
-window.onload = () => {
-    // Header scroll functionality
-    let scrollpos = window.scrollY
-    const nav = document.getElementById('nav')
-    const burger = document.getElementById('burger')
-    const nav_height = nav.offsetHeight
-    const menu = document.getElementById("menu")
-
-    const add_class_on_scroll = () => {
-        nav.classList.add("nav--scroll")
-        burger.classList.add("burger-container--scroll")
-    }
-    const remove_class_on_scroll = () => {
-        nav.classList.remove("nav--scroll")
-        burger.classList.remove("burger-container--scroll")
-    }
-
-    window.addEventListener('scroll', function() { 
-        scrollpos = window.scrollY;
-        if (scrollpos >= nav_height) { 
-            add_class_on_scroll() 
-        }
-        else { 
-            remove_class_on_scroll() 
-        }
-    })
-
-    // Burger open close functionality  
-    if(burger) {
-      var navItems = document.getElementsByClassName("nav__item")
-  
-      function openMenu() {
-        menu.classList.add("show");
-        burger.classList.add("burger-container--close");
-      }
-  
-      function closeMenu() {
-        menu.classList.remove("show");
-        burger.classList.remove("burger-container--close");
-      }
-  
-      function toggleMenu() {
-        if (burger.classList.contains('burger-container--close')) {
-          closeMenu();
-        }
-        else if (!burger.classList.contains('burger-container--close')) {
-          openMenu();
-        }
-      }
-  
-      burger.onclick = toggleMenu
-      for (let i = 0; i < navItems.length; i++){
-          navItems[i].onclick = closeMenu;
-      }
-    }
-  };
-  
-
-export default Header
--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -14,5 +14,14 @@ const Layout = (props) => {
         </div>
     )
 }
+if (typeof window !== "undefined") {
+    // eslint-disable-next-line global-require
+    require("smooth-scroll")('a[href*="#"]', {
+        speed: 700,
+        speedAsDuration: true,
+        easing: 'easeInOutCubic'
+    })
+    
+}
 
 export default Layout
