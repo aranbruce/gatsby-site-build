ab2653deb5401b4de02c5fe953228c62958f0c27
Aran <aranbruce@gmail.com>
2019-06-29 12:24:42 +0100

    removed page transitions as had bugs

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,16 +1,10 @@
 import React, { Component } from "react"
-import posed, { PoseGroup } from "react-pose"
 
 import Head from "../components/head/head"
 import Header from "../components/header/header"
 import Footer from "../components/footer/footer"
 
 import "../styles/styles.sass"
-
-const FadeContent = posed.div({
-  enter: { opacity: 1, transition: { duration: 500 } },
-  exit: { opacity: 0, transition: { duration: 500 } },
-})
 
 class Layout extends Component {
   state = { isVisible: false }
@@ -35,16 +29,7 @@ class Layout extends Component {
           pageURL={this.props.pageURL}
         />
         <Header />
-        <PoseGroup>
-          {this.state.isVisible && [
-            <FadeContent
-              pose={this.state.isVisible ? "visible" : "hidden"}
-              key="FadeContent"
-            >
-              {this.props.children}
-            </FadeContent>,
-          ]}
-        </PoseGroup>
+        {this.props.children}
         <Footer />
       </div>
     )
